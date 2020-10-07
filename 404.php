2e3018fec4ce77b854f8eec1498e360dca55c15f
Erpreet <preetkaurchd15@gmail.com>
2020-10-07 11:37:01 -0600

    404, archive.php

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<h2> Error:404 </h2>
+
+<p>
+The page or post you requested was not found.
+
+<a href="<?php echo site_url(); ?>">
+Click here to return to homepage
+</a>
+and try again. Thanks!
+</p>
+
+<?php get_footer();
---
+++ footer.php
@@ -0,0 +1,20 @@
+<footer>
+<h2><?php bloginfo('title'); ?> Footer </h2>
+<p>
+&copy;
+<?php echo date('Y'); ?>
+<a href="<?php echo site_url(); ?>">
+<?php bloginfo('title'); ?>
+</a>
+All rights reserved.
+</p>
+</footer>
+
+
+
+<?php
+wp_footer();
+?>
+
+</body>
+</html>
